6298fa24068ffc52fae5c1ab04841f81d8079415
Farid Eldokany <2449308e@student.gla.ac.uk>
2021-08-15 14:51:26 +0200

    Updated Bluetooth sensor
    -updated mac address
    -updated desired uuid

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from btle import BTLEDisconnectError
 while(True):
     try:
         # Creates a new bluetooth object based on MAC address of device and the desired characteristic UUID of the device.
-        sensor= Bluetooth("c1:bf:3c:b2:1b:f1", '00002a5b-0000-1000-8000-00805f9b34fb')
+        sensor= Bluetooth("e2:24:ee:4f:cb:6a", '00002a63-0000-1000-8000-00805f9b34fb')
         # Connects to device using random connection mode.
         sensor.connect("random")
         # Subscribes to the UUID to be capable on getting notifications later on.
